e3006bd2f6acdf0fb3e2407395cbc228b4a918f5
Nils Magnus <nils.magnus@t-systems.com>
2021-04-14 23:34:18 +0000

    meet the wishes of GitHub for commas in CSV files

--- tools/01_download.sh
+++ tools/01_download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 grep -v '^#' ../whitelist/list.csv |\
-    awk -F\; '{print $1, $2}' | while read id url; do
+    awk -F\, '{print $1, $2}' | while read id url; do
         echo -n "$id "
         curl -s $(echo $url | sed 's/.*github.com/https:\/\/raw.githubusercontent.com/;s/\/blob//') -o data/file-$id
     done
